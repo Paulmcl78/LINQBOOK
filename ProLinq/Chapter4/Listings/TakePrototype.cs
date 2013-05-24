65904a337ef64943b064ade2be15a97bc5126f8c
Paul McLaughlin <pmclaughlin@navinet.net>
2013-05-24 13:48:31 +0100

    Take Prototype

---
+++ ProLinq/Chapter4/Listings/TakePrototype.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace Chapter4.Listings
+{
+    /// <summary>
+    /// This prototype specifies that Take will receive an input source sequence
+    /// </summary>
+    public class TakePrototype :IListing 
+    {
+        public void Print()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
